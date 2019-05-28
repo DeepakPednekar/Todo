f5e8bce8ad81bf3602fb1591515e5483ffc26eee
Deepak Pednekar <deepak.pednekar009@gmail.com>
2019-05-28 20:26:12 +0530

    list-group added

--- src/app/todo-items/todo-list.component.ts
+++ src/app/todo-items/todo-list.component.ts
@@ -1,9 +1,16 @@
 import {Component} from '@angular/core';
+import {TodoServices} from './todo.services';
+import {TodoListItem} from './todo-items.interfaces';
 
 @Component({
   selector: 'app-todoitem-list',
   templateUrl: './todo-list.component.html'
 })
 export class TodoListComponent{
+  todoList: TodoListItem[] = []; // this holds local reference for list items
 
+  constructor(private s_todo: TodoServices){
+
+    this.todoList = this.s_todo.todoList;
+  }
 }
--- src/app/todo-items/todo.services.ts
+++ src/app/todo-items/todo.services.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import {TodoListItem} from './todo-items.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoServices {
+  todoList: TodoListItem[] = [];
 
 }
