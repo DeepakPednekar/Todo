1f79f9ea7e6bd5ccf43ac5c966124e6d5a092bff
Deepak Pednekar <deepak.pednekar009@gmail.com>
2019-08-08 15:36:08 +0530

    formgroup added in login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {UserService} from './user.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -7,7 +9,37 @@ import {UserService} from './user.service';
 })
 export class LoginComponent {
 
-  constructor(private s_user: UserService) {}
+  form_login: FormGroup;
+  constructor(private s_user: UserService, private s_router: Router) {
+    this.form_login = new FormGroup({
+      username : new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required, Validators.minLength(6)])
+      });
 
+  } // end of constructor
+
+  onSubmit(){
+    const value = this.form_login.getRawValue();
+
+    this.s_user.authentication(value).subscribe( ret_data => {
+
+      if(ret_data.type == '+OK'){
+        let token = ret_data['token'];
+        this.s_router.navigate(['/']);
+        localStorage.setItem('token', token);
+      }
+
+    });
+
+  }
+
+  // control getters
+  get fc_username(){
+    return <FormControl>this.form_login.controls.username;
+  }
+
+  get fc_password(){
+    return <FormControl>this.form_login.controls.password;
+  }
 
 }
